cd94e18188adcace6631a41aeaaa9101e184eeb9
sb2333 <33334154+sb2333@users.noreply.github.com>
2017-11-03 15:51:26 -0400

    Add files via upload

--- logs_analysis.py
+++ logs_analysis.py
@@ -1,7 +1,7 @@
 """Project: LOGS ANALYSIS"""
 
 
-# /usr/bin/python2.7.13
+# /usr/bin/python 2.7.13
 #
 #
 
@@ -10,13 +10,13 @@ import psycopg2
 
 def connect():
 
-	"""Try to connect to database news"""
-        try:
-                db = psycopg2.connect("dbname='news'")
-                c = db.cursor()
-                return db, c
-        except:
-		print "I am unable to connect to the database."
+    """Try to connect to database news"""
+    try:
+        db = psycopg2.connect("dbname='news'")
+        c = db.cursor()
+    return db, c
+    except:
+        print "I am unable to connect to the database."
 
 # 1. What are the most popular three articles of all time?
 # Create pop_article_view
@@ -47,7 +47,6 @@ select date(time) as date, count(*) as total
 from log
 group by date
 order by total desc
-
 Errors Total  view:
 create view errors_total as
 select date(time) as date, count(*) as total
@@ -55,7 +54,6 @@ from log
 where status != '200 OK'
 group by date
 order by total desc
-
 Error Percent view:
 create view percent_error as
 select r.date, round((100.00*(e.total)/r.total),3) as percent_error
@@ -67,46 +65,51 @@ SQL_query_1 = "select * from pop_article_view limit 3"
 SQL_query_2 = "select * from pop_authors_view"
 SQL_query_3 = " select * from percent_error as e where e.percent_error > 1"
 
-def most_pop_articles(SQL_query_1):
-        """Query and print results from the view created for most popular articles
-	"""
 
-	db, c = connect()
-	c.execute(SQL_query_1)
-	results = c.fetchall()
-	for i in range(0, len(results), 1):
-                print "\"" + results[i][0] + "\" - " + str(results[i][1]) + " views"
-        db.close()
+def most_pop_articles(SQL_query_1):
+
+    """Query and print results from the view created for most popular\
+    articles"""
+
+    db, c = connect()
+    c.execute(SQL_query_1)
+    results = c.fetchall()
+    for i in range(0, len(results), 1):
+        print "\"" + results[i][0] + "\" - " + str(results[i][1]) + " views"
+    db.close()
+
 
 def most_pop_authors(SQL_query_2):
-        """Query and print the results of popular authors view created for most popular\
-        authors of all time"""
 
-        db, c = connect()
-        c.execute(SQL_query_2)
-        results = c.fetchall()
-        for i in range(0, len(results), 1):
-                print "\"" + results[i][0] + "\" - " + str(results[i][1]) + " views"
+    """Query and print the results of popular authors view created for\
+    most popular authors of all time"""
+
+    db, c = connect()
+    c.execute(SQL_query_2)
+    results = c.fetchall()
+    for i in range(0, len(results), 1):
+        print "\"" + results[i][0] + "\" - " + str(results[i][1]) + "views"
         db.close()
 
 
 def percent_error(SQL_query_3):
-        """Query and print results from percent errors view for days on which more than 1%\
-        of requests lead to errors"""
 
-        db, c = connect()
-        c.execute(SQL_query_3)
-        results = c.fetchall()
-        for i in range(0, len(results), 1):
-                print str(results[i][0])+ "\" - "+ str(results[i][1]) + "% errors"
-        db.close()
+    """Query and print results from percent errors view for days on which\
+    more than 1% of requests lead to errors"""
+
+    db, c = connect()
+    c.execute(SQL_query_3)
+    results = c.fetchall()
+    for i in range(0, len(results), 1):
+        print str(results[i][0]) + "\" - " + str(results[i][1]) + "% errors"
+    db.close()
 
 if __name__ == '__main__':
-	print "\n The results for Most Popular Articles are:\n"
-	most_pop_articles(SQL_query_1)
-	print ("\n")
-	print "\n The results for Most Popular Authors are:\n"
-	most_pop_authors(SQL_query_2)
-	print ("\n")
-	print "\n The results for days with more than 1% of errors are:\n"
-	percent_error(SQL_query_3)
+    print "\n The results for Most Popular Articles are:\n"
+    most_pop_articles(SQL_query_1)
+    print ("\n")
+    print "\n The results for Most Popular Authors are:\n"
+    most_pop_authors(SQL_query_2)
+    print ("\n")
+    print "\n The results for days with more than 1% of errors are:\n"
+    percent_error(SQL_query_3)
